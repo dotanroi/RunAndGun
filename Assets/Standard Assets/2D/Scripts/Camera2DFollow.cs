52530a01841d3774f4643ca0918d10be732bc0e3
DotanTabTale <dotanr@tabtale.com>
2015-06-06 20:28:15 +0300

    add scene

--- Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -45,6 +45,8 @@ namespace UnityStandardAssets._2D
             Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
             Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
 
+			newPos.y=transform.position.y;
+
             transform.position = newPos;
 
             m_LastTargetPosition = target.position;
